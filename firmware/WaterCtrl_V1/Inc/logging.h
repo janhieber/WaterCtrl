0337a7b814a8ca3daee4ce9d68082486ad9e2acf
Jan Hieber <info@janhieber.net>
2015-10-08 18:22:37 +0200

    add logging code files

---
+++ firmware/WaterCtrl_V1/Inc/logging.h
@@ -0,0 +1,21 @@
+/* ---------------------------------------------------------------------------
+** This software is in the public domain, furnished "as is", without technical
+** support, and with no warranty, express or implied, as to its usefulness for
+** any purpose.
+**
+** logging.h
+** Functions for logging messages and routing/filter them.
+**
+** Author: Jan Hieber <mail@janhieber.net>
+** -------------------------------------------------------------------------*/
+
+#ifndef LOGGING_H
+#define LOGGING_H
+
+
+
+
+
+
+
+#endif // LOGGING_H
---
+++ firmware/WaterCtrl_V1/Src/logging.c
@@ -0,0 +1,10 @@
+/* ---------------------------------------------------------------------------
+** This software is in the public domain, furnished "as is", without technical
+** support, and with no warranty, express or implied, as to its usefulness for
+** any purpose.
+**
+** logging.c
+** *see header file*
+**
+** Author: Jan Hieber <mail@janhieber.net>
+** -------------------------------------------------------------------------*/
