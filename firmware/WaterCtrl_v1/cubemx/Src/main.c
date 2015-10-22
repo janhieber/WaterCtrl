6a50c1b53ef4a52215aa19c496a32c28915e72ca
Jan Hieber <info@janhieber.net>
2015-10-23 00:15:36 +0200

    add independent watchdog, en/disable in mxconstants.h

--- firmware/WaterCtrl_v1/cubemx/Src/main.c
+++ firmware/WaterCtrl_v1/cubemx/Src/main.c
@@ -6,33 +6,26 @@
   *
   * COPYRIGHT(c) 2015 STMicroelectronics
   *
-  * Redistribution and use in source and binary forms, with or without
-  *modification,
+  * Redistribution and use in source and binary forms, with or without modification,
   * are permitted provided that the following conditions are met:
   *   1. Redistributions of source code must retain the above copyright notice,
   *      this list of conditions and the following disclaimer.
-  *   2. Redistributions in binary form must reproduce the above copyright
-  *notice,
-  *      this list of conditions and the following disclaimer in the
-  *documentation
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
   *      and/or other materials provided with the distribution.
-  *   3. Neither the name of STMicroelectronics nor the names of its
-  *contributors
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
   *      may be used to endorse or promote products derived from this software
   *      without specific prior written permission.
   *
   * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
   * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-  *ARE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
   * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
   * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
   * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
   * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-  *LIABILITY,
-  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
-  *USE
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
   * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   *
   ******************************************************************************
@@ -46,24 +39,17 @@
   */
 
 /* ============ DEFINES ============== */
-// general
+// GENERAL
 #define VERSION (uint8_t)1
-
-// for scheduler
-#define INTERVAL_ALWAYS 0
-#define INTERVAL_10MS 10
-#define INTERVAL_100MS 100
-#define INTERVAL_500MS 500
-#define INTERVAL_1S 1000
-#define INTERVAL_5S 5000
-
-#define ARRAYSIZE(x) (sizeof(x) / sizeof((x)[0]))
 
 // TOOLCHAIN INCLUDES
 #include <stdint.h>
 #include <stdbool.h>
 
-// INCLUDES
+// APP INCLUDES
+#include <mxconstants.h>
+#include <stuff.h>
+#include <scheduler.h>
 #include <spicomm.h>
 #include <log.h>
 #include <moistureMeasure.h>
@@ -72,6 +58,8 @@
 /* USER CODE END Includes */
 
 /* Private variables ---------------------------------------------------------*/
+IWDG_HandleTypeDef hiwdg;
+
 SPI_HandleTypeDef hspi1;
 
 TIM_HandleTypeDef htim2;
@@ -87,6 +75,7 @@ UART_HandleTypeDef huart1;
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_IWDG_Init(void);
 static void MX_SPI1_Init(void);
 static void MX_TIM2_Init(void);
 static void MX_TIM3_Init(void);
@@ -94,9 +83,6 @@ static void MX_USART1_UART_Init(void);
 
 /* USER CODE BEGIN PFP */
 /* Private function prototypes -----------------------------------------------*/
-void AliveTicker(void);
-void Tsk_Always(void);
-void Tsk_10ms(void);
 
 /* USER CODE END PFP */
 
@@ -105,55 +91,25 @@ void Tsk_10ms(void);
 /*! this is for printf.c to handle output */
 void PrintChar(char c) { HAL_UART_Transmit(&huart1, (uint8_t*)&c, 1, 50); }
 
-/*! Define parameters to configure a task */
-typedef struct {
-  uint16_t Interval;   // how often the task will run
-  uint32_t LastTick;   // stores last tick the task was ran
-  void (*Func)(void);  // function ptr to task
-} TaskType;
+/*!
+ * @brief task for resetting watchdog
+ */
+void resetIWDG(void) {
+  WatchdogReset(&hiwdg);
+}
 
-/*! Task configuration table */
-static TaskType Tasks[] = {
-    {INTERVAL_ALWAYS, 0, Tsk_Always},
-    {INTERVAL_10MS, 0, Tsk_10ms},
+/*! Scheduler task configuration table */
+TaskType Tasks[] = {
+    {INTERVAL_1S, 0, resetIWDG},
     {INTERVAL_1S, 0, AliveTicker},
     {INTERVAL_5S, 0, printMoisture},
 };
 
-/**
- * @brief Example tasks that runs as often as possible
- */
-void Tsk_Always(void) { __NOP(); }
-
-/**
- * @brief Example task that runs every 10 ms
- */
-void Tsk_10ms(void) { __NOP(); }
-
-/**
- * @brief System alive indicator for LED
- */
-void AliveTicker(void) {
-  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
-  HAL_Delay(50);
-  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
-}
-
-/**
- * @brief Get number of scheduled tasks
- * @return Number of tasks
- */
-uint8_t Tsk_GetNumTasks(void) { return ARRAYSIZE(Tasks); }
-
-/**
- * @brief Get task configuration table
- * @return Task configuration table
- */
-TaskType* Tsk_GetConfig(void) { return Tasks; }
-
 /* USER CODE END 0 */
 
-int main(void) {
+int main(void)
+{
+
   /* USER CODE BEGIN 1 */
 
   // this is for temporary sprintf stuff
@@ -162,11 +118,9 @@ int main(void) {
   };
   /* USER CODE END 1 */
 
-  /* MCU
-   * Configuration----------------------------------------------------------*/
+  /* MCU Configuration----------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick.
-   */
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
 
   /* Configure the system clock */
@@ -174,6 +128,7 @@ int main(void) {
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_IWDG_Init();
   MX_SPI1_Init();
   MX_TIM2_Init();
   MX_TIM3_Init();
@@ -195,64 +150,56 @@ int main(void) {
   initMoistureMeasure(&htim3);
   initMotors();
 
-  // setup scheduler vars
-  static uint32_t tick = 0;
-  static TaskType* TaskPtr;
-  static uint8_t TaskIndex = 0;
-  const uint8_t NumTasks = Tsk_GetNumTasks();
-  sprintf(tmpbuf, "Scheduler: %d tasks scheduled", NumTasks);
-  Log(LogDebug, tmpbuf);
-
-  // init scheduler
-  TaskPtr = Tsk_GetConfig();
+  // init scheduling
+  initScheduler(Tasks, ARRAYSIZE(Tasks));
 
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+
   Log(LogDebug, "reaching mainloop");
   while (1) {
+    // start watchdog
+    WatchdogStart(&hiwdg);
+
     // scheduling
-    tick = HAL_GetTick();
-    for (TaskIndex = 0; TaskIndex < NumTasks; TaskIndex++) {
-      if (TaskPtr[TaskIndex].Interval == 0)
-        (*TaskPtr[TaskIndex].Func)();
-      else if ((tick - TaskPtr[TaskIndex].LastTick) >=
-               TaskPtr[TaskIndex].Interval) {
-        (*TaskPtr[TaskIndex].Func)();
-        TaskPtr[TaskIndex].LastTick = tick;
-      }
-    }
+    doScheduling();
 
-    /* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
+  /* USER CODE END WHILE */
+
+  /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
+
 }
 
 /** System Clock Configuration
 */
-void SystemClock_Config(void) {
+void SystemClock_Config(void)
+{
+
   RCC_OscInitTypeDef RCC_OscInitStruct;
   RCC_ClkInitTypeDef RCC_ClkInitStruct;
 
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSI|RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
   HAL_RCC_OscConfig(&RCC_OscInitStruct);
 
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_SYSCLK|RCC_CLOCKTYPE_PCLK1;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
   HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2);
 
-  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq() / 1000);
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
 
   HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
 
@@ -260,8 +207,21 @@ void SystemClock_Config(void) {
   HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
 }
 
+/* IWDG init function */
+void MX_IWDG_Init(void)
+{
+
+  hiwdg.Instance = IWDG;
+  hiwdg.Init.Prescaler = IWDG_PRESCALER_16;
+  hiwdg.Init.Reload = 4095;
+  HAL_IWDG_Init(&hiwdg);
+
+}
+
 /* SPI1 init function */
-void MX_SPI1_Init(void) {
+void MX_SPI1_Init(void)
+{
+
   hspi1.Instance = SPI1;
   hspi1.Init.Mode = SPI_MODE_SLAVE;
   hspi1.Init.Direction = SPI_DIRECTION_2LINES;
@@ -275,10 +235,13 @@ void MX_SPI1_Init(void) {
   hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLED;
   hspi1.Init.CRCPolynomial = 10;
   HAL_SPI_Init(&hspi1);
+
 }
 
 /* TIM2 init function */
-void MX_TIM2_Init(void) {
+void MX_TIM2_Init(void)
+{
+
   TIM_ClockConfigTypeDef sClockSourceConfig;
   TIM_MasterConfigTypeDef sMasterConfig;
   TIM_OC_InitTypeDef sConfigOC;
@@ -304,10 +267,13 @@ void MX_TIM2_Init(void) {
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
   HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_2);
+
 }
 
 /* TIM3 init function */
-void MX_TIM3_Init(void) {
+void MX_TIM3_Init(void)
+{
+
   TIM_MasterConfigTypeDef sMasterConfig;
   TIM_IC_InitTypeDef sConfigIC;
 
@@ -327,10 +293,13 @@ void MX_TIM3_Init(void) {
   sConfigIC.ICPrescaler = TIM_ICPSC_DIV8;
   sConfigIC.ICFilter = 3;
   HAL_TIM_IC_ConfigChannel(&htim3, &sConfigIC, TIM_CHANNEL_3);
+
 }
 
 /* USART1 init function */
-void MX_USART1_UART_Init(void) {
+void MX_USART1_UART_Init(void)
+{
+
   huart1.Instance = USART1;
   huart1.Init.BaudRate = 115200;
   huart1.Init.WordLength = UART_WORDLENGTH_8B;
@@ -340,19 +309,21 @@ void MX_USART1_UART_Init(void) {
   huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
   huart1.Init.OverSampling = UART_OVERSAMPLING_16;
   HAL_UART_Init(&huart1);
+
 }
 
-/** Configure pins as
-        * Analog
-        * Input
+/** Configure pins as 
+        * Analog 
+        * Input 
         * Output
         * EVENT_OUT
         * EXTI
-        * Free pins are configured automatically as Analog (this feature is
- * enabled through
+        * Free pins are configured automatically as Analog (this feature is enabled through 
         * the Code Generation settings)
 */
-void MX_GPIO_Init(void) {
+void MX_GPIO_Init(void)
+{
+
   GPIO_InitTypeDef GPIO_InitStruct;
 
   /* GPIO Ports Clock Enable */
@@ -368,7 +339,7 @@ void MX_GPIO_Init(void) {
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PC14 PC15 */
-  GPIO_InitStruct.Pin = GPIO_PIN_14 | GPIO_PIN_15;
+  GPIO_InitStruct.Pin = GPIO_PIN_14|GPIO_PIN_15;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
@@ -379,29 +350,30 @@ void MX_GPIO_Init(void) {
   GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PA2 PA3 PA4 PA8
+  /*Configure GPIO pins : PA2 PA3 PA4 PA8 
                            PA11 PA12 PA15 */
-  GPIO_InitStruct.Pin = GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_8 |
-                        GPIO_PIN_11 | GPIO_PIN_12 | GPIO_PIN_15;
+  GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_8 
+                          |GPIO_PIN_11|GPIO_PIN_12|GPIO_PIN_15;
   GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PB1 PB10 PB11 PB15
+  /*Configure GPIO pins : PB1 PB10 PB11 PB15 
                            PB9 */
-  GPIO_InitStruct.Pin =
-      GPIO_PIN_1 | GPIO_PIN_10 | GPIO_PIN_11 | GPIO_PIN_15 | GPIO_PIN_9;
+  GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_15 
+                          |GPIO_PIN_9;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PB2 PB12 PB13 PB14
-                           PB3 PB4 PB5 PB6
+  /*Configure GPIO pins : PB2 PB12 PB13 PB14 
+                           PB3 PB4 PB5 PB6 
                            PB7 PB8 */
-  GPIO_InitStruct.Pin = GPIO_PIN_2 | GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 |
-                        GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 |
-                        GPIO_PIN_7 | GPIO_PIN_8;
+  GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14 
+                          |GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6 
+                          |GPIO_PIN_7|GPIO_PIN_8;
   GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
 }
 
 /* USER CODE BEGIN 4 */
@@ -419,23 +391,25 @@ void MX_GPIO_Init(void) {
    * @param line: assert_param error line source number
    * @retval None
    */
-void assert_failed(uint8_t* file, uint32_t line) {
+void assert_failed(uint8_t* file, uint32_t line)
+{
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line
      number,
        ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line)
      */
   /* USER CODE END 6 */
+
 }
 
 #endif
 
 /**
   * @}
-  */
+  */ 
 
 /**
   * @}
-*/
+*/ 
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
